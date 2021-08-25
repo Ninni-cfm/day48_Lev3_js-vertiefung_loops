cc0d196505569e66064b337d7fb85317b31351b6
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-08-25 14:49:17 +0200

    Complete

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # day48_Lev3_js-vertiefung_loops
+
+**JS Vertiefung CodeFlow Übung lev3_1: for loop string Array - Aufgabenstellung**
+
+-   Erstelle eine Variable mit einem Text (siehe Kommentarbereich)
+-   Erstelle eine Funktion, die einen Text in einem Abstand von 100 Zeichen trennt.
+-   Füge nach den 100 Zeichen “ - 1 of gesamte-Nachrichten”, “ - 2 of gesamte-Nachrichten” usw … hinzu.
+-   Jedes Element dieser Zeichenkette (100 Zeichen + - 1 of x ) soll in ein Array gepusht werden.
+
+Gib dir am Ende das Ergebnis in der Konsole aus.
+
+Rufe die Funktion mit der Variablen als Argument auf.
+
+---
+
+**JS Vertiefung CodeFlow Übung lev3_2: for loop Zahlen in Array teilen - Aufgabenstellung**
+
+Du hast ein Array aus Zahlen und möchtest herausfinden, durch welche Zahlen sie (außer durch eins und sich selbst) teilbar sind.
+
+Schreibe das Ergebnis in dein HTML.
+
+---
+
+_Source:_ https://github.com/Ninni-cfm/day48_Lev3_js-vertiefung_loops
+
+_Demo:_ https://ninni-cfm.github.io/day48_Lev3_js-vertiefung_loops/
---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+var show = (text = undefined, tag = 'p') => {
+    document.write(text != undefined ? `<${tag}>${text}</${tag}>` : '<br>');
+    console.log(text != undefined ? text : '');
+}
+var showArray = (arr) => {
+    console.log(arr);
+    document.write(`<p>(${arr.length}) [ `);
+
+    if (typeof (elt) == "Number") {
+        document.write(arr.join(', '));
+    }
+    else {
+        document.write(`"${arr.join('", "')}"`);
+    }
+    document.write(' ]</p>');
+}
+
+//****************************************************************************
+// CodeFlow Übung lev3_1: for loop string Array
+// Erstelle eine Variable mit einem Text (siehe Kommentarbereich)
+// Erstelle eine Funktion, die einen Text in einem Abstand von 100 Zeichen 
+// trennt.
+// Füge nach den 100 Zeichen “ - 1 of gesamte-Nachrichten”, “ - 2 of
+// gesamte-Nachrichten” usw … hinzu.
+// Jedes Element dieser Zeichenkette (100 Zeichen + - 1 of x ) soll in ein
+//  Array gepusht werden.
+// Gib dir am Ende das Ergebnis in der Konsole aus.
+// Rufe die Funktion mit der Variablen als Argument auf.
+show('CodeFlow Übung lev3_1: for loop string Array', 'h2');
+let text = `I didn't ask for a completely reasonable excuse! I asked you to get busy! But I know you in the future. I cleaned your poop. What are you hacking off? Is it my torso?! 'It is!' My precious torso! Please, Don-Bot… look into your hard drive, and open your mercy file! Shut up and get to the point! I barely knew Philip, but as a clergyman I have no problem telling his most intimate friends all about him. You know the worst thing about being a slave? They make you work, but they don't pay you or let you go. Moving along… Well, then good news! It's a suppository. I'll get my kit! And when we woke up, we had these bodies. Daddy Bender, we're hungry. You know the worst thing about being a slave? They make you work, but they don't pay you or let you go. Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. Alright, let's mafia things up a bit. Joey, burn down the ship. Clamps, burn down the crew. That could be 'my' beautiful soul sitting naked on a couch. If I could just learn to play this stupid thing. Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that panders to my lust for violence? I was all of history's great robot actors - Acting Unit 0.8; Thespomat; David Duchovny! Bender, I didn't know you liked cooking. That's so cute. Fatal. You don't know how to do any of those. Who are you, my warranty?! Shut up and get to the point! Fry, you can't just sit here in the dark listening to classical music. That's not soon enough! And why did 'I' have to take a cab? Bender?! You stole the atom. You know, I was God once. For the last time, I don't like lilacs! Your 'first' wife was the one who liked lilacs! And yet you haven't said what I told you to say! How can any of us trust you? Your best is an idiot! I found what I need. And it's not friends, it's things. No! The kind with looting and maybe starting a few fires! When will that be? Ugh, it's filthy! Why not create a National Endowment for Strip Clubs while we're at it? I'm just glad my fat, ugly mama isn't alive to see this day. Why did you bring us here? And until then, I can never die? I'm sure those windmills will keep them cool. You don't know how to do any of those. Fry, you can't just sit here in the dark listening to classical music. You know the worst thing about being a slave? They make you work, but they don't pay you or let you go. This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Oh, but you can. But you may have to metaphorically make a deal with the devil. And by "devil", `
+
+function splitText(maxLength) {
+    let maxParts = Math.floor(text.length / maxLength) + (text.length % maxLength == 0 ? 0 : 1);
+    let parts = [];
+    for (let i = 0; i < maxParts; i++) {
+        let part = text.substr(i * maxLength, maxLength);
+        part += ` - ${i + 1} of ${maxParts}`;
+        parts.push(part);
+    }
+    return parts;
+}
+splitText(100).forEach(part => show(part));
+show();
+
+
+//****************************************************************************
+// CodeFlow Übung lev3_2: for loop Zahlen in Array teilen
+// Du hast ein Array aus Zahlen und möchtest herausfinden, durch welche Zahlen
+// sie (außer durch eins und sich selbst) teilbar sind.
+// Schreibe das Ergebnis in dein HTML.
+show('CodeFlow Übung lev3_2: for loop Zahlen in Array teilen', 'h2');
+let numArr = [5, 22, 15, 100, 55]
+numArr.forEach(num => {
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i == 0)
+            show(`${num} laesst sich durch ${i} teilen! Das Ergenis ist ${num / i}`)
+    }
+
+
+});
+
